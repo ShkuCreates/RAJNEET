e9abd9eab51e93482065426cac173cdb6eec556f
ShkuCreates <mail.shouryaaa@gmail.com>
2026-05-11 07:30:23 +0530

    Config: Add Cloudinary and news image sources to next.config.js

--- next.config.js
+++ next.config.js
@@ -18,6 +18,18 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.newsdata.io',
+      }
     ],
   },
 }
