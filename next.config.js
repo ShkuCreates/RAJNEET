759f7ded99c31d431d048da8e79311f570e409cd
ShkuCreates <mail.shouryaaa@gmail.com>
2026-05-15 19:33:43 +0530

    Fix Render deploy: disable Turbopack and remove old eslint config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   images: {
@@ -32,6 +23,7 @@ const nextConfig = {
       }
     ],
   },
+  turbo: false,
 }
 
 module.exports = nextConfig
